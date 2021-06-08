5d622699cc292f8eb7e294e46be38042e2ea1d28
soramkirtep <soramkirtep@gmail.com>
2021-06-08 21:08:18 +0200

    play with another branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there!!!
+          I was changed on feature branch!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
